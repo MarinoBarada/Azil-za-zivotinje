0ef2f4a41f0ee6bac985bca21a89131297dd995d
MarinoBarada <marino18barada@gmail.com>
2023-05-04 15:38:44 +0200

    Postavljanje API-ja

--- src/api/azilData.js
+++ src/api/azilData.js
@@ -1,3 +1,7 @@
 import axios from "axios";
 
 export const getAllNotifications = () => axios.get("/obavijesti");
+
+export const createNotification = (data) => axios.post("/obavijesti", data);
+
+export const deleteNotificationById = (id) => axios.delete(`/obavijesti/${id}`);
